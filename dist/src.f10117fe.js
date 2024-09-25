56b7d38c03948f1ad7315be4419b83ea2b90476b
Lance <lance@lancesnider.com>
2024-09-25 13:15:38 -0700

    Delete cache files

--- dist/src.f10117fe.js
+++ dist/src.f10117fe.js
@@ -4698,7 +4698,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62481" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63038" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
